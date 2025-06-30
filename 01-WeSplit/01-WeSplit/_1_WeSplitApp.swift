062e6c9a7c9863090a0021de932bee3dd99f71cf
Amber Jen <amberjen226@gmail.com>
2025-06-30 21:18:11 +0800

    Day 16-18 Project 1

---
+++ 01-WeSplit/01-WeSplit/_1_WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  _1_WeSplitApp.swift
+//  01-WeSplit
+//
+//  Created by Amber Jen on 2025/6/28.
+//
+
+import SwiftUI
+
+@main
+struct _1_WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
