f958f48c5bcc036758913aee62ede626065f3301
Amber Jen <amberjen226@gmail.com>
2025-07-11 22:09:36 +0800

    Day 92-94 Project 18

--- 07-LayoutAndGeometry/07-LayoutAndGeometry/ContentView.swift
+++ 07-LayoutAndGeometry/07-LayoutAndGeometry/ContentView.swift
@@ -9,25 +9,35 @@ import SwiftUI
 
 struct ContentView: View {
     
-    let colors:[Color] = [.red, .orange, .yellow, .green, .blue, .indigo, .purple]
+    let colors: [Color] = (0..<7).map { i in
+        Color(hue: 0.7, saturation: 0.5 + (Double(i) * 0.07), brightness: 1.0 - (Double(i) * 0.05))
+    }
     
     var body: some View {
         
         GeometryReader { fullView in
             ScrollView {
                 
-                ForEach(0..<50) { index in
+                ForEach(0..<75) { index in
                     GeometryReader { proxy in
+                    
+                        let yPosition = proxy.frame(in: .global).minY
+                        let opacity = yPosition < 120 ? yPosition / 120 : 1.0
+                        let scale = 0.5 + (yPosition / fullView.size.height) * 0.6
+                        
                         Text("Row \(index)")
-                            .font(Font.title)
+                            .font(Font.title2)
                             .frame(maxWidth: .infinity)
+                            .padding(16)
                             .background(colors[index % 7])
                             .rotation3DEffect(
-                                .degrees((proxy.frame(in: .global).minY - fullView.size.height / 2) / 5.0),
+                                .degrees((proxy.frame(in: .global).minY - fullView.size.height / 2) / 4.0),
                                 axis: (x: 0, y: 1, z: 0))
-                            
+                            .opacity(opacity)
+                            .scaleEffect(scale)
+                        
                     }
-                    .frame(height: 48)
+                    .frame(height: 44)
                 }
                 
             } // END: scrollview
