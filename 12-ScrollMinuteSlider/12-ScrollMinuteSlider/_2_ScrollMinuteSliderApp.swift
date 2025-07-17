23a1ee64ddfc7cc68dbb88bbbf49134e29da48b0
Amber Jen <amberjen226@gmail.com>
2025-07-18 07:58:40 +0800

    12 WIP

---
+++ 12-ScrollMinuteSlider/12-ScrollMinuteSlider/_2_ScrollMinuteSliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  _2_ScrollMinuteSliderApp.swift
+//  12-ScrollMinuteSlider
+//
+//  Created by Amber Jen on 2025/7/18.
+//
+
+import SwiftUI
+
+@main
+struct _2_ScrollMinuteSliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
