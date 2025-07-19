c513222ca0c1a09a3a66faabcb411a082aed6e1b
Amber Jen <amberjen226@gmail.com>
2025-07-19 20:15:01 +0800

    ...

--- Playground/Playground/ContentView.swift
+++ Playground/Playground/ContentView.swift
@@ -8,17 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+
+    @State private var change = false
+    
     var body: some View {
+        
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+            
+        } // v
+       
+      
     }
 }
 
 #Preview {
     ContentView()
+        .preferredColorScheme(.dark)
 }
