a6da2f45d7591f53f4d972c5eed5f6759d644e87
Amber Jen <amberjen226@gmail.com>
2025-07-22 07:57:22 +0800

    Play~

--- Playground/Playground/ContentView.swift
+++ Playground/Playground/ContentView.swift
@@ -9,19 +9,20 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var change = false
+     @State private var change = false
     
     var body: some View {
         
         
-        VStack(spacing: 24){
+        VStack(spacing: 24) {
+             
             
             Button("Change") {
                 change.toggle()
             }
             
-        } // v
-        .padding(24)
+        }
+        
        
       
     }
