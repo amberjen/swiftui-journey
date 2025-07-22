f36b15c661d147b0aaa1833b29c6ec5a573b43eb
Amber Jen <amberjen226@gmail.com>
2025-07-22 22:01:53 +0800

    Project 13

--- 13-DynamicTab/13-DynamicTab/ContentView.swift
+++ 13-DynamicTab/13-DynamicTab/ContentView.swift
@@ -10,126 +10,75 @@ import SwiftUI
 struct TabModel: Identifiable {
     var id = UUID()
     var icon: String
+    var iconFill: String
     var title: String
+    var selectedColor: Color
 }
 
 struct ContentView: View {
     
-    @State private var selectedTab = 0
+    @State private var selectedTab = 2
     
     var tabs: [TabModel] = [
-        TabModel(icon: "person.fill", title: "Primary"), // 0
-        TabModel(icon: "cart.fill", title: "Transactions"), // 1
-        TabModel(icon: "sparkles", title: "Updates"), // 2
-        TabModel(icon: "tag.fill", title: "Promotions") // 3
+        TabModel(icon:"person", iconFill: "person.fill", title: "Primary", selectedColor: .pink), // Tab 0
+        TabModel(icon:"cart", iconFill: "cart.fill", title: "Cart", selectedColor: .indigo), // Tab 1
+        TabModel(icon:"creditcard", iconFill: "creditcard.fill", title: "Payment", selectedColor: .blue), // Tab 2
+        TabModel(icon:"tag", iconFill: "tag.fill", title: "Promo", selectedColor: .brown) // Tab 3
     ]
     
     var body: some View {
         
-        GeometryReader { geo in
-            
-            HStack(spacing: 0) {
-                // 0
-                view1()
-                    .containerRelativeFrame(.horizontal, count: 1, spacing: 0)
-                    
-                // 1
-                view2()
-                    .containerRelativeFrame(.horizontal, count: 1, spacing: 0)
-                
-                // 2
-                view3()
-                    .containerRelativeFrame(.horizontal, count: 1, spacing: 0)
-                
-                // 3
-                view4()
-                    .containerRelativeFrame(.horizontal, count: 1, spacing: 0)
-            
-                
-            } // H
-            .offset(x: -geo.size.width * 0)
-            
-            
-        } // GR
-        .overlay(alignment: .center) {
-            HStack {
-                ForEach(tabs.indices, id:\.self) { tab in
-                    
-                    HStack {
-                        Image(systemName: tabs[tab].icon)
-                        
-                        if selectedTab == tab {
-                            Text(tabs[tab].title).bold()
-                                .transition(.scale(scale: 0, anchor: .trailing).combined(with: .opacity))
-                        }
-                    }
-                    .frame(maxWidth: selectedTab == tab ? .infinity : 56)
-                    .frame(height: 56)
-                    .background(Color.white.opacity(0.15))
-                    .cornerRadius(16)
-                    .clipped()
-                    .onTapGesture {
-                        withAnimation {
-                            selectedTab = tab
-                        }
-                    } // tapgesture
-                    
-                } // foreach
-            } // h
-            
-            
-        } // overlay
-        .padding(24)
-    
-        
-    }
-}
-
-struct view1: View {
-    var body: some View {
         ZStack {
+            
             Color.black
                 .ignoresSafeArea()
             
-//            Text("View 1")
-//                .font(.title)
-        } // z
-    }
-}
-
-struct view2: View {
-    var body: some View {
-        ZStack {
-            Color.indigo
-                .ignoresSafeArea()
+            VStack {
+                
+                HStack {
+                    ForEach(tabs.indices, id:\.self) { tab in
+                        
+                        HStack {
+                            Image(systemName: selectedTab == tab ? tabs[tab].iconFill : tabs[tab].icon)
+                                .font(.system(size: 18, weight: .medium))
+                                .foregroundStyle(selectedTab == tab ? .white : .white.opacity(0.5))
+                            
+                            if selectedTab == tab {
+                                Text(tabs[tab].title).bold()
+                                    .font(.system(size: 18, weight: .bold))
+                                    .transition(
+                                        .asymmetric(
+                                            insertion: .scale(scale: 0.95, anchor: .trailing).combined(with: .opacity),
+                                            removal: .opacity
+                                        )
+                                        
+                                    )
+                            }
+                        }
+                        .font(.title3)
+                        .frame(maxWidth: selectedTab == tab ? .infinity : 56)
+                        .frame(height: 56)
+                        .background(
+                            selectedTab == tab ? tabs[tab].selectedColor : Color.white.opacity(0.1)
+                        )
+                        .cornerRadius(16)
+                        .onTapGesture {
+                            withAnimation(.spring(duration: 0.25, bounce: 0.35)) {
+                                selectedTab = tab
+                            }
+                        }
+                        
+                    } // ForEach
+                    
+                } // H
+                
+                
+            } // V
+            .padding(24)
             
-            Text("View 2")
-                .font(.title)
-        } // z
-    }
-}
-
-struct view3: View {
-    var body: some View {
-        ZStack {
-            Color.blue
-                .ignoresSafeArea()
             
-            Text("View 3")
-                .font(.title)
-        } // z
-    }
-}
-
-struct view4: View {
-    var body: some View {
-        ZStack {
-            Color.cyan
-                .ignoresSafeArea()
-            
-            Text("View 4")
-                .font(.title)
-        } // z
+        }
+        
     }
 }
 
